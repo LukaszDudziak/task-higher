a82be2b3b227c98c6908e79ee7877f2bad20ed95
dudziakpudziak <lukaszdudziak777@gmail.com>
2020-09-27 17:04:36 +0200

    working_1.0

--- main.js
+++ main.js
@@ -1,41 +1,53 @@
-const imageDispay = document.querySelector(".imageDispay");
-const arrayResult = [];
+const imageDispay = document.querySelector(".imageDisplay");
+const nextButton = document.getElementById("nextBtn");
 let numberOfImage = 3;
 let count = 0;
 
-//adding new image from response
+//append new image from response
 const appendNewImage = (response) => {
-  for (count; count < numberOfImage; count++) {
+  //hide section at the start (no one need to see how those images jumps when rendered)
+  imageDispay.style.opacity = 0;
+  //create next 3 containers filled with image element and description (author's name)
+  for (i = count; i < numberOfImage; i++) {
     let newElement = document.createElement("img");
+    let newContainer = document.createElement("figure");
+    let newFigcaption = document.createElement("figcaption");
+    newContainer.classList.add("container");
+    newFigcaption.innerText = `fot.${response[i].author}`;
     //substring url, to get slug
-    let url = response[count].url.substr(28);
-    let author = response[count].author;
-    //creating new image
-    newElement.src = `http://source.unsplash.com/${url}`;
-    newElement.alt = author;
-    imageDispay.appendChild(newElement);
+    let slug = response[i].url.substr(28);
+    //render new image
+    newElement.src = `http://source.unsplash.com/${slug}`;
+    imageDispay.appendChild(newContainer);
+    newContainer.appendChild(newElement);
+    newContainer.appendChild(newFigcaption);
   }
+  //finally show effect
+  setTimeout(function () {
+    imageDispay.style.opacity = 1;
+  }, 1000);
 };
 
-//fetching
-const fetching = fetch("https://picsum.photos/v2/list")
+//fetch
+fetch("https://picsum.photos/v2/list")
   .then((response) => {
+    //return response in json object
     return response.json();
   })
   .then((response) => {
-    console.log(response);
     appendNewImage(response);
+    //create listener for button
+    nextButton.addEventListener("click", () => {
+      count = count + 3;
+      numberOfImage = numberOfImage + 3;
+      //loop next button
+      if (numberOfImage == response.length) {
+        count = 0;
+        numberOfImage = 3;
+      }
+      //clear images section
+      imageDispay.innerHTML = "";
+      //run function which will set new images
+      appendNewImage(response);
+    });
   });
-
-//WERSJA Z ITEROWANIEM PO CAŁOŚCI
-// response.forEach((resp) => {
-//   count++;
-//   let newElement = document.createElement("img");
-//   //substring url, to get slug
-//   let url = resp.url.substr(28);
-//   //creating new image
-//   if (count <= numberOfImage) {
-//     newElement.src = `http://source.unsplash.com/${url}`;
-//     imageDispay.appendChild(newElement);
-//   }
-// });
