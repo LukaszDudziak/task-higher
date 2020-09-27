3e4bb377c4c3f4c2b2ec3a37e04a2b12348d1e47
dudziakpudziak <lukaszdudziak777@gmail.com>
2020-09-27 13:42:36 +0200

    200927

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 const imageDispay = document.querySelector(".imageDispay");
 const arrayResult = [];
-let url;
-let newElement;
+let numberOfImage = 3;
+let count = 0;
 
-fetch("https://picsum.photos/v2/list")
+//adding new image from response
+const appendNewImage = (response) => {
+  for (count; count < numberOfImage; count++) {
+    let newElement = document.createElement("img");
+    //substring url, to get slug
+    let url = response[count].url.substr(28);
+    let author = response[count].author;
+    //creating new image
+    newElement.src = `http://source.unsplash.com/${url}`;
+    newElement.alt = author;
+    imageDispay.appendChild(newElement);
+  }
+};
+
+//fetching
+const fetching = fetch("https://picsum.photos/v2/list")
   .then((response) => {
     return response.json();
   })
   .then((response) => {
     console.log(response);
-
-    response.forEach((resp) => {
-      newElement = document.createElement("img");
-      console.log(resp.url);
-      url = resp.url.substr(28);
-      console.log(url);
-      newElement.src = `http://source.unsplash.com/${url}`;
-      imageDispay.appendChild(newElement);
-    });
+    appendNewImage(response);
   });
+
+//WERSJA Z ITEROWANIEM PO CAŁOŚCI
+// response.forEach((resp) => {
+//   count++;
+//   let newElement = document.createElement("img");
+//   //substring url, to get slug
+//   let url = resp.url.substr(28);
+//   //creating new image
+//   if (count <= numberOfImage) {
+//     newElement.src = `http://source.unsplash.com/${url}`;
+//     imageDispay.appendChild(newElement);
+//   }
+// });
